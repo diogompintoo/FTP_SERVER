fec222a642d2eee164c88106661f4ea5fc589902
diogompintoo <diogomartinsp1992@gmail.com>
2026-04-30 12:54:30 +0100

    add utility package with default port

---
+++ src/main/java/Server/Commands.java
@@ -0,0 +1,42 @@
+package Server;
+
+public enum Commands {
+
+    HELP("HELP", "Show all commands"),
+    LS("LS", "List all files on server"),
+    MKDIR("MKDIR", ""),
+    GET("GET", ""),
+    PUT("PUT", ""),
+    DELETE("DELETE", ""),
+    QUIT("QUIT", ""),
+    BYE("BYE", ""),
+    DISCONNECT("DISCONNECT", ""),
+    UNKNOWN("UNKNOWN", "");
+
+    private final String commandText;
+    private final String description;
+
+    Commands(String commandText, String description) {
+        this.commandText = commandText;
+        this.description = description;
+    }
+
+    public String getCommandText() {
+        return commandText;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Commands getCommand(String text) {
+
+
+            for (Commands cmn : Commands.values()) {
+                if (cmn.getCommandText().equals(text)) {
+                    return cmn;
+                }
+            }
+            return UNKNOWN;
+        }
+}
---
+++ src/main/java/Utility/Constants.java
@@ -0,0 +1,7 @@
+package Utility;
+
+public class Constants {
+
+    public static final int DEFAULT_PORT = 9999;
+   // public static final int String SERVER_ROOT = "serverRoot"
+}
